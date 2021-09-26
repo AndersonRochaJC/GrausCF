28366cf77d2b02d467a567f3d0b61b9ea783b063
AndersonRochaJC <jmcreggae@gmail.com>
2021-09-26 14:08:03 -0300

    conclusão de exercicio

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace GrausCF
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double fahren, graus;
+            Console.BackgroundColor = ConsoleColor.Green;
+            Console.WriteLine(" conversor de graus Fahrenheit para Graus Celsius \n ");
+            Console.BackgroundColor = ConsoleColor.Red;
+            Console.Write(" Digite a temperatura em Fahrenheit: ");            
+            fahren = Convert.ToDouble(Console.ReadLine());
+            graus = (fahren - 32) / 1.8;
+            Console.WriteLine($" {fahren}°F equivalente a {graus}°C ");
+            Console.ResetColor( );
+        }
+    }
+}
